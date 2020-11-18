02aa205f6a4c15ded95a35b7b6595790f40bed94
Michal <michalnoga.poczta@gmail.com>
2020-11-18 15:41:31 +0100

    Darts

---
+++ java/darts/src/main/java/Darts.java
@@ -0,0 +1,23 @@
+class Darts {
+
+    private double x;
+    private double y;
+
+    Darts(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    int score() {
+        double distance = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+
+        if (distance <= 1) {
+            return 10;
+        } else if (distance <= 5) {
+            return 5;
+        } else if (distance <= 10) {
+            return 1;
+        }
+        return 0;
+    }
+}
