d99486e0e78e86ba58e55e9503cf3b94d52e5df4
Michal <michal@google.com>
2022-11-04 19:12:22 +0100

    Three numbers

---
+++ C++/1.1-three-numbers/three_numbers.cpp
@@ -0,0 +1,10 @@
+#include "three_numbers.h"
+#include <iostream>
+#include <algorithm>
+
+int main() {
+    int first, second, third;
+
+    std::cout << "Enter first number: ";
+    std::cin >> first;
+}
---
+++ C++/1.1-three-numbers/three_numbers.h
@@ -0,0 +1,13 @@
+//
+// Created by micha on 21/09/2025.
+//
+
+#ifndef C___THREE_NUMBERS_H
+#define C___THREE_NUMBERS_H
+
+
+class three_numbers {
+};
+
+
+#endif //C___THREE_NUMBERS_H
