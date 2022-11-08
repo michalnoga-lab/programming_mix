d7a57e7999a0b3503dcb5874a682f72432886c6f
Michal <michal@google.com>
2022-11-08 19:39:59 +0100

    Three numbers

--- C++/1.1-three-numbers/three_numbers.cpp
+++ C++/1.1-three-numbers/three_numbers.cpp
@@ -2,9 +2,38 @@
 #include <iostream>
 #include <algorithm>
 
+void print_numbers(int number_1, int number_2, int iterations);
+
 int main() {
     int first, second, third;
 
     std::cout << "Enter first number: ";
     std::cin >> first;
+
+    std::cout << "Enter second number: ";
+    std::cin >> second;
+
+    std::cout << "Enter third number: ";
+    std::cin >> third;
+
+    int biggest = std::max({first, second, third});
+    std::cout << "Biggest number is: " << biggest << std::endl;
+
+    if (first < biggest && second < biggest) {
+        print_numbers(first, second, biggest);
+    }
+    if (first < biggest && third < biggest) {
+        print_numbers(first, third, biggest);
+    }
+    if (second < biggest && third < biggest) {
+        print_numbers(second, third, biggest);
+    }
+
+    return 0;
+}
+
+void print_numbers(int number_1, int number_2, int iterations) {
+    for (int i = 0; i < iterations; i++) {
+        std::cout << number_1 + number_2 << std::endl;
+    }
 }
