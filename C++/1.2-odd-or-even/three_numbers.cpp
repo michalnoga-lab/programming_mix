240be249b96cf8b9a498eec2333789260df4f4b9
Michal <michal@google.com>
2022-12-08 19:30:27 +0100

    Odd or Even

--- C++/1.2-odd-or-even/three_numbers.cpp
+++ C++/1.2-odd-or-even/three_numbers.cpp
@@ -1,39 +1,14 @@
 #include "three_numbers.h"
 #include <iostream>
-#include <algorithm>
 
-void print_numbers(int number_1, int number_2, int iterations);
 
 int main() {
-    int first, second, third;
+    int user_input;
 
-    std::cout << "Enter first number: ";
-    std::cin >> first;
+    std::cout << "Enter an integer: ";
+    std::cin >> user_input;
 
-    std::cout << "Enter second number: ";
-    std::cin >> second;
-
-    std::cout << "Enter third number: ";
-    std::cin >> third;
-
-    int biggest = std::max({first, second, third});
-    std::cout << "Biggest number is: " << biggest << std::endl;
-
-    if (first < biggest && second < biggest) {
-        print_numbers(first, second, biggest);
-    }
-    if (first < biggest && third < biggest) {
-        print_numbers(first, third, biggest);
-    }
-    if (second < biggest && third < biggest) {
-        print_numbers(second, third, biggest);
-    }
+    user_input % 2 == 0 ? std::cout << "YES" : std::cout << "NO";
 
     return 0;
-}
-
-void print_numbers(int number_1, int number_2, int iterations) {
-    for (int i = 0; i < iterations; i++) {
-        std::cout << number_1 + number_2 << std::endl;
-    }
 }
